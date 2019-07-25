0f1d018e0c37fe9ec281a93aade88bdbf48c7e87
brent <jinhoshin319@gmail.com>
2019-07-25 21:21:57 +0900

    initial setup화면 추가 중,

--- app/src/main/java/com/example/lovedays/Screens/InitialSetupScreen.java
+++ app/src/main/java/com/example/lovedays/Screens/InitialSetupScreen.java
@@ -1,10 +1,15 @@
 package com.example.lovedays.Screens;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,10 +18,21 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.example.lovedays.MainActivity;
 import com.example.lovedays.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by KING JINHO on 2019-07-24
  */
 public class InitialSetupScreen extends AbsFragment {
+
+    private Button mBtnClear;
+    private Button mBtnStart;
+    private TextView mTvRelationshipDate;
+    private EditText mEtMyName;
+    private EditText mEtHisHerName;
+    private EditText mEtMobileNumber;
 
     @Override
     public void onAttach(Context context) {
@@ -29,12 +45,28 @@ public class InitialSetupScreen extends AbsFragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);//안하면 mDefaultView null.
         ConstraintLayout layout = mDefaultView.findViewById(R.id.fragment_container);
         View view = inflater.inflate(R.layout.initialsetupscreen, container, false);
+        final Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int dayOfMonth = cal.get(Calendar.DATE);
+        mBtnStart = view.findViewById(R.id.btnStart);
+        mBtnClear = view.findViewById(R.id.btnClear);
+        mTvRelationshipDate = view.findViewById(R.id.tvDateSelect);
+        mTvRelationshipDate.setText(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
+        mTvRelationshipDate.setOnClickListener(v-> {
+            DatePickerDialog dialog = new DatePickerDialog(root, R.style.DialogTheme, (datePicker, yearSelected, monthSelected, dateSelected) ->{
+                Toast.makeText(root, "its all good", Toast.LENGTH_SHORT).show();
+            }, year, month, dayOfMonth);
+
+            dialog.show();
+        });
         layout.addView(view);
         return mDefaultView;
     }
